fab3ce5cf26fb1d81d9b1c447ec66472e765e97c
Max <maxvdboom1@outlook.com>
2020-05-28 12:10:21 +0200

    Timer set up in temperatuur programma

--- LuchtTemperatuur/LuchtTemperatuur.ino
+++ LuchtTemperatuur/LuchtTemperatuur.ino
@@ -1,19 +1,45 @@
 #include <dht.h>
+#include <SimpleTimer.h>
+
+//timer object
+SimpleTimer timer;
 
 dht DHT;
 
 #define DHT11_PIN 7
+int ledPin = 6; 
+
 
 void setup(){
   Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  
+  timer.setInterval(10000, TimerTest);
 }
 
 void loop()
 {
-  int chk = DHT.read11(DHT11_PIN);
+    timer.run();
+}
+
+
+void TimerTest()
+{
+
+  //checkt de data van de sensor en zet deze in de serial monitor
+  //dit doet hij elke 10 seconden door de timer die ik heb ingesteld. 
+  
+  int chk = DHT.read11(DHT11_PIN);  
   Serial.print("Temperature = ");
   Serial.println(DHT.temperature);
   Serial.print("Humidity = ");
   Serial.println(DHT.humidity);
-  delay(1000);
+
+  //ledje gaat elke 10 seconden aan en na 10 seconden weer uit
+  //dit is voor visualisatie zodat ik kan zien of de timer werkt
+  if(digitalRead(ledPin) == HIGH){
+  digitalWrite(ledPin, LOW);
+  } else{
+  digitalWrite(ledPin, HIGH);
+  }
 }
